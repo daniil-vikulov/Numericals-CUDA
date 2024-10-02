6ced7a005ba71622415b03ae660158f82425f320
= <=>
2024-10-02 20:47:29 +0300

    Add random system generation

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -3,6 +3,8 @@
 #include "CUDASolver.h"
 
 #include <iostream>
+#include <random>
+#include <algorithm>
 
 void gpuSolve() {
     const std::vector<std::vector<double> > a = {
@@ -28,8 +30,36 @@ void gpuSolve() {
     std::cout << ")" << std::endl;
 }
 
+void gpuSolveRandom() {
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_real_distribution<double> dist(-10000.0, 10000.0);
+
+    constexpr int n = 10000;
+    std::vector<std::vector<double> > a(n, std::vector<double>(n, 0));
+    for (auto &row: a) {
+        std::ranges::generate(row, [&]() { return static_cast<double>(dist(rng)); });
+    }
+
+    std::vector<double> b(n);
+    std::ranges::generate(b, [&]() { return static_cast<double>(dist(rng)); });
+
+    CUDASolver solver(a, 1, b);
+    auto x = solver.solve();
+
+    std::cout << "Solution vector x:" << std::endl;
+    std::cout << "(";
+    for (size_t i = 0; i < x.size(); ++i) {
+        std::cout << std::format("{:.8f}", x[i]);
+        if (i < x.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << ")" << std::endl;
+}
+
 int main() {
-    gpuSolve();
+    gpuSolveRandom();
 
     return 0;
 }
