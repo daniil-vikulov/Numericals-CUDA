481d29b194cf4c8f13725080e19ff7feb8dd78c9
lizzinni <l9273143@gmail.com>
2024-11-05 02:52:24 +0300

    gauss done

---
+++ include/gauss.hpp
@@ -0,0 +1,17 @@
+#ifndef GAUSS_METHOD_HPP
+#define GAUSS_METHOD_HPP
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+class gauss {
+private:
+    int N;
+public:
+    gauss(int N_) : N(N_) {}
+    static void solve_slae_gauss(std::vector<std::vector<double>> A, std::vector<double> b, std::vector<double> &x);
+    static void solve_slae_lapack(std::vector<std::vector<double>> &A, std::vector<double> &b, std::vector<double> &x);
+};
+
+#endif // GAUSS_METHOD_HPP
