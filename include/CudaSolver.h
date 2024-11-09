49a72902c464c3238bb155a28b351cae018bca0c
daniil-vikulov <daniel.vikulov@yandex.ru>
2024-11-09 18:37:03 +0100

    Solve initial

--- include/CudaSolver.h
+++ include/CudaSolver.h
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <cublas_v2.h>
+
 class CudaSolver {
-private:
-    float *x_k, r_k, r_k_prev, p_k, b;
-    int n;
+    const int n{};
+    cublasHandle_t handle{};
+    int k{};
+    float *A{};
+    float *x_k{};
+    float *r_k{};
+    float *p_k{};
+    float *b{};
+    float *r_k_new{};
+    float *tmp{};
+    static constexpr float one = 1.0f;
+    static constexpr float zero = 0.0f;
 
 public:
     ///@brief instantiates CUDA tool, which finds a vector X in the following equation: AX = B, where A is a known
@@ -12,8 +23,20 @@ public:
 
     ~CudaSolver();
 
-    void solve(float *A, float *b, int maxIterations, float tolerance, float *res);
+    bool solve(const float *A, const float *b, int maxIterations, float tolerance, float *res);
 
 private:
-    float iterate();
+    [[nodiscard]] float iterate() const;
+
+    [[nodiscard]] inline float calc_alpha() const;
+
+    inline void update_x(float alpha) const;
+
+    inline void update_r_k_new(float alpha) const;
+
+    [[nodiscard]] inline float calc_beta() const;
+
+    inline void update_p(float beta) const;
+
+    inline float dot(const float *v1, const float *v2) const;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,61 @@
-#include <iostream>
 #include "CudaSolver.h"
+#include <iostream>
+#include <cassert>
+
+#define N 3
+
+float* unwrap(float A[N][N]) {
+    const auto A_flat = new float[N * N];
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            A_flat[i * N + j] = A[i][j];
+        }
+    }
+
+    return A_flat;
+}
+
+float* multiply(const float* A, const float* x, int n) {
+    // Allocate memory for the result vector Ax
+    auto Ax = new float[n];
+
+    // Perform the matrix-vector multiplication
+    for (int i = 0; i < n; ++i) {
+        Ax[i] = 0.0f;
+        for (int j = 0; j < n; ++j) {
+            Ax[i] += A[i * n + j] * x[j];
+        }
+    }
+
+    return Ax;
+}
 
 int main () {
+    CudaSolver solver(N);
+
+    float A[N][N] = {{4, 1, 1}, {1, 3, 0}, {1, 0, 2}};
+    float b[N] = {10, -8, 12};
+
+    float res[N] = {};
+
+    const auto A_1D = unwrap(A);
+
+    assert(solver.solve(A_1D, b, 100, 1e-7, res));
+    multiply(A_1D, res, N);
+
+    for (const auto el : res){
+        std::cout << el << " ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < N; ++i) {
+        std::cout << res[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] A_1D;
+    //delete[] product;
 
     return 0;
 }
